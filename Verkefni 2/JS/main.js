b3bfe9c767468c553efcb4f7837fccc0b901c114
Olafurjon <olafurjonb@hotmail.com>
2017-09-07 16:34:56 +0000

    tæknilega séð virkar nema er ekki optimal

--- Verkefni 2/JS/main.js
+++ Verkefni 2/JS/main.js
@@ -3,57 +3,80 @@ var ctx = c.getContext("2d");
 var motion = "null";
 var x = 10;
 var y = 10
-var z = 55;
-rect1 = ctx.fillRect(x, y, z, 50);
+var newx = x;
+var newy = x;
+var z = 0;
+var pos = [];
+var xchecker = [];
+var ychecker = [];
+var path = [];
+
+rect1 = ctx.fillRect(x, y, 50, 50);
 
 var applealive = false; 
 
-
-
-
 var imgepli = document.getElementById("eplimynd");
   
-
-
 function getmyapple()
 {
+
   if(applealive == false)
   {
 
-  var randpos = [Math.floor((Math.random() * c.height)), Math.floor((Math.random() * c.height))];
-  epli =  ctx.drawImage(imgepli, randpos[0], randpos[1],50,50);
-  applealive = true;
+  var randpos = [Math.floor((Math.random() * (c.height-50))), Math.floor((Math.random() * (c.height-50)))];
+  pos = randpos
+  
 
-  if(x == randpos[0] && y == randpos[1] )
+}
+}
+
+function drawapple(posx,posy)
+{
+  applealive = true;
+  epli =  ctx.drawImage(imgepli, posx,posy,50,50);
+  if(xchecker.includes(posx) && ychecker.includes(posy))
   {
+    path.splice(0,2*z);
     console.log("collide");
+  
+    z++;
     applealive = false;
   }
-}
+  }
+function drawrect()
+{   ctx.fillRect(x, y, 50, 50);
+
+    for (var i =1; i < z; i++) {
+       
+      ctx.fillRect(path[path.length - i][0], path[path.length - i][1], 50, 50);
+     
+    
+    }
+     
+
 }
 
 window.document.addEventListener("keydown",function(e)
 {
 
 
+  if (e.key == "d") {
+   
+    motion = "right";
 
-if (e.key == "d") {
- 
-  motion = "right";
+  }
+  if (e.key == "a") {
+     motion = "left";
+  }
 
-}
-if (e.key == "a") {
-   motion = "left";
-}
+  if (e.key == "s") {
+    motion = "down";
+  }
+  if (e.key == "w") {
+    motion = "up";
+  }
 
-if (e.key == "s") {
-  motion = "down";
-}
-if (e.key == "w") {
-  motion = "up";
-}
-
-console.log(motion);
+  console.log(motion);
 
 });
 
@@ -66,51 +89,60 @@ function moveitmoveit(motion)
 { 
 
 
+  xchecker = []
+  var negativex = x
+  var negativey = y
+for (var i = 0; i < 30; i++) {
+  xchecker.push(x + i);
+  xchecker.push(negativex - i);
+};
+  ychecker = []
+for (var i = 0; i < 30; i++) {
+  ychecker.push(y + i);
+  ychecker.push(negativey - i);
+};
+getmyapple();
+drawapple(pos[0],pos[1]);
 
   if(motion != "null")
   {
-    epli =  ctx.drawImage(imgepli, 150,150,50,50);
-
-    if(x == 150 && y == 150 )
-    {
-      console.log("collide");
-      applealive = false;
-    }
     
-
+    
     if(motion == "right")
     {
     ctx.clearRect(0,0,c.width,c.height);
     x = x + speed;
-    rect1 = ctx.fillRect(x, y, z, 50);
+  
+    drawrect()
+    drawapple(pos[0],pos[1])
     }
 
      if(motion == "left")
     {
     ctx.clearRect(0,0,c.width,c.height);
     x = x - speed;
-    rect1 = ctx.fillRect(x, y, z, 50);
+  
+    drawrect()
+    drawapple(pos[0],pos[1])
     }
 
     if(motion == "down")
     {
     ctx.clearRect(0,0,c.width,c.height);
     y = y + speed;
-    rect1 = ctx.fillRect(x, y, z, 50);
+
+    drawrect()
+    drawapple(pos[0],pos[1])
     }
 
     if(motion == "up")
     {
     ctx.clearRect(0,0,c.width,c.height);
     y = y - speed;
-    rect1 = ctx.fillRect(x, y, z, 50);
+    drawrect()
+    drawapple(pos[0],pos[1])
     }
     
-    
-   
-
-
-
   }
 
 
@@ -126,7 +158,21 @@ while(motion != "null");
   {
 
    moveitmoveit(motion);
-    
+
+     if(!path.indexOf([x,y]))
+      {
+        
+    }
+    else
+    {
+      path.push([x,y]);
+    }
+
+
+   console.log(path.length);
+
+
+
   },speedup)
 }
 
